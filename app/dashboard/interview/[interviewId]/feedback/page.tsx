95489bb91abb5dae492e93c395d6c603c15d2060
Afreen Shaik <afreens@reccopilot.com>
2024-10-28 18:21:29 +0530

    modified feedback

--- app/dashboard/interview/[interviewId]/feedback/page.tsx
+++ app/dashboard/interview/[interviewId]/feedback/page.tsx
@@ -67,54 +67,44 @@ const Feedback: React.FC<FeedbackProps> = ({ params }) => {
 
   return (
     <div className="p-10">
-      {feedbackList.length === 0 ? (
-        <h2 className="fold-bold text-xl text-gray-500">
-          No Interview feedback Record found
-        </h2>
-      ) : (
-        <>
-          <h2 className="text-3xl font-bold text-green-500">
-            Congratulations!
-          </h2>
-          <h2 className="font-bold text-2xl text-gray-600">
-            Here is your Interview Feedback
-          </h2>
-          <h2 className="text-primary text-lg my-10">
-            Your overall interview rating:{" "}
-            <strong>
-              {averageRating ? `${averageRating.toFixed(1)}/10` : "N/A"}
-            </strong>
-          </h2>
+      <h2 className="text-3xl font-bold text-green-500">Congratulations!</h2>
+      <h2 className="font-bold text-2xl text-gray-600">
+        Here is your Interview Feedback
+      </h2>
+      <h2 className="text-primary text-lg my-10">
+        Your overall interview rating:{" "}
+        <strong>
+          {averageRating ? `${averageRating.toFixed(1)}/10` : "N/A"}
+        </strong>
+      </h2>
 
-          <h2 className="text-sm text-gray-500">
-            Find below interview question with correct answer, Your answer and
-            feedback for improvement
-          </h2>
-          {feedbackList.map((item, index) => (
-            <Collapsible key={index} className="mt-7">
-              <CollapsibleTrigger className="p-2 bg-secondary rounded-lg my-2 text-left flex justify-between gap-7 w-full">
-                {item.question} <ChevronsUpDown className="h-5 w-5" />
-              </CollapsibleTrigger>
-              <CollapsibleContent>
-                <div className="flex flex-col gap-2">
-                  <h2 className="p-2 text-red-400 border rounded-lg">
-                    <strong>Rating:</strong> {item.rating}
-                  </h2>
-                  <h2 className="p-2 border rounded-lg bg-red-50 text-sm text-red-900">
-                    <strong>User Answer: </strong> {item.userAns}
-                  </h2>
-                  <h2 className="p-2 border rounded-lg bg-green-50 text-sm text-green-900">
-                    <strong>Correct Answer: </strong> {item.correctAns}
-                  </h2>
-                  <h2 className="p-2 border rounded-lg bg-blue-50 text-sm text-blue-900">
-                    <strong>Feedback: </strong> {item.feedback}
-                  </h2>
-                </div>
-              </CollapsibleContent>
-            </Collapsible>
-          ))}
-        </>
-      )}
+      <h2 className="text-sm text-gray-500">
+        Find below interview question with correct answer, Your answer and
+        feedback for improvement
+      </h2>
+      {feedbackList.map((item, index) => (
+        <Collapsible key={index} className="mt-7">
+          <CollapsibleTrigger className="p-2 bg-secondary rounded-lg my-2 text-left flex justify-between gap-7 w-full">
+            {item.question} <ChevronsUpDown className="h-5 w-5" />
+          </CollapsibleTrigger>
+          <CollapsibleContent>
+            <div className="flex flex-col gap-2">
+              <h2 className="p-2 text-red-400 border rounded-lg">
+                <strong>Rating:</strong> {item.rating}
+              </h2>
+              <h2 className="p-2 border rounded-lg bg-red-50 text-sm text-red-900">
+                <strong>User Answer: </strong> {item.userAns}
+              </h2>
+              <h2 className="p-2 border rounded-lg bg-green-50 text-sm text-green-900">
+                <strong>Correct Answer: </strong> {item.correctAns}
+              </h2>
+              <h2 className="p-2 border rounded-lg bg-blue-50 text-sm text-blue-900">
+                <strong>Feedback: </strong> {item.feedback}
+              </h2>
+            </div>
+          </CollapsibleContent>
+        </Collapsible>
+      ))}
       <Button
         onClick={() => router.replace("/dashboard")}
         className="mt-10 bg-primary text-white transition-opacity duration-300 hover:opacity-80 hover:bg-primary"
